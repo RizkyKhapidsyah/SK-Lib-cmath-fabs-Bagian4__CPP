ac03efc4c963017ed3907b9e7b7a67dc7bb38a76
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-08 10:19:27 +0700

    Add project files.

---
+++ SK-Lib-cmath-fabs-Bagian4__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+/*
+    Source by Programiz
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+int main() {
+    double x = -10.25, hasil;
+
+    hasil = fabs(x);
+    cout << "fabs(" << x << ") = |" << x << "| = " << hasil << endl;
+
+    _getch();
+    return 0;
+}
